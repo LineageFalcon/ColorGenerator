ccf5d21c1791bef91ad77c7304ce6dc07d6a5229
Malcom Cook <68928999+LineageFalcon@users.noreply.github.com>
2021-01-02 02:14:30 +0100

    📝: refactored the color convertion
    and its effects to the DOM

--- dynamics.js
+++ dynamics.js
@@ -1,90 +1,149 @@
-function RanColorGen(PnlID) {
-    const HexChars = '0123456789ABCDEF';
-    let ColorOne = '#';
+document.addEventListener("DOMContentLoaded", function() {
+    new colorConvert();
+});
+class colorConvert {
+    constructor() {
+        this._hexChars = null
+        
+        colorConvert.setHexChars( '0123456789ABCDEF');
+    }
+
+    //#region Setter
+    static setHexChars(chars) {
+        this._hexChars = chars;
+    }
+
+    static getHexChars() {
+        return this._hexChars;
+    }
+    //#endregion Setter
+
+    static ranColorGen(range) {
+        const HEXCHARS = colorConvert.getHexChars();
+        let ranColor = '#';
+
+        for(let i = 6; i > 0; i--) {
+            ranColor += HEXCHARS[Math.floor(Math.random() * 16)];
+        }
+
+        return ranColor;
+    }
+
+    static rgbStringToHex(rgb) {
+        rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        let hex = '#';
+
+        for(let i = 1; i < 4; i++) {
+           hex += ("0" + parseInt(rgb[i]).toString(16)).slice(-2);
+        }
+
+        return hex;
+    }
+
+    static rgbToHex(rgb) {
+        let r = rgb[0].toString(16),
+            g = rgb[1].toString(16),
+            b = rgb[2].toString(16);
+
+        if (r.length == 1)
+          r = "0" + r;
+        if (g.length == 1)
+          g = "0" + g;
+        if (b.length == 1)
+          b = "0" + b;
+      
+        return "#" + r + g + b;
+    }
+
+    static hexToRgbString(hex) {
+        let r = 0, g = 0, b = 0;
+
+        // 3 digits
+        if (hex.length == 4) {
+        r = "0x" + hex[1] + hex[1];
+        g = "0x" + hex[2] + hex[2];
+        b = "0x" + hex[3] + hex[3];
+    
+        // 6 digits
+        } else if (hex.length == 7) {
+        r = "0x" + hex[1] + hex[2];
+        g = "0x" + hex[3] + hex[4];
+        b = "0x" + hex[5] + hex[6];
+        }
+    
+        return "rgb(" + r + "," + g + "," + b + ")";
+    }
+
+    static hexToRgb(hex) {
+        let r = 0, g = 0, b = 0;
+
+        // 3 digits
+        if (hex.length == 4) {
+        r = "0x" + hex[1] + hex[1];
+        g = "0x" + hex[2] + hex[2];
+        b = "0x" + hex[3] + hex[3];
+    
+        // 6 digits
+        } else if (hex.length == 7) {
+        r = "0x" + hex[1] + hex[2];
+        g = "0x" + hex[3] + hex[4];
+        b = "0x" + hex[5] + hex[6];
+        }
+    
+        return [r, g, b];
+    }
+
+    static changeLightness(color, lightValue) {
+        for(let i = 0; i < 3; i++) {
+            if (color[i] !== 0 && color[i] >= lightValue) {
+                color[i] -= lightValue;
+            } else if (color[i] === color[i]) {
+                color[i] = 0;
+            }
+        }
+
+        return color;
+    }
+}
+
+class themeUI {
+    constructor() {
+
+    }
+}
+
+function mainColor(pnlID) {
+    let ColorOne = colorConvert.ranColorGen();
     let ColorTwo;
     let ColorTwoTxt;
     
-    for (let i = 0; i < 6; i++) {
-        ColorOne += HexChars[Math.floor(Math.random() * 16)];
-    }
+    ColorTwo = colorConvert.changeLightness(colorConvert.hexToRgb(ColorOne), 50);
+    ColorTwoTxt =colorConvert.rgbToHex(ColorTwo);
     
-    ColorTwo = alphaEdit(ColorOne);
-    ColorTwoTxt = rgb2hex(ColorTwo);
-    
-    distributer(ColorOne, ColorTwo, ColorTwoTxt, PnlID);
+    distributer(ColorOne, ColorTwoTxt, pnlID);
 }
 
-function distributer(ColorOne, ColorTwo, ColorTwoTxt, PnlID) {
+function distributer(ColorOne, ColorTwoTxt, PnlID) {
     $(PnlID).each(function() {
         let selector = $(this).children();
 
         selector.filter('.colorOne').css('background-color', ColorOne);
-        selector.filter('.colorTwo').css('background-color', ColorTwo);
+        selector.filter('.colorTwo').css('background-color', ColorTwoTxt);
 
         selector.filter('section').find('p').text(ColorOne);
         selector.filter('#containerCTwo').find('p').text(ColorTwoTxt);
     });
 }
 
-const hexDigits = new Array
-    ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
-
-//Function to convert rgb color to hex format
-function rgb2hex(rgb) {
-    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
-    }
-    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-}
-
-function hex(x) {
-    return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-}
- 
-function alphaEdit(hex) {
-    let r = 0, g = 0, b = 0;
-
-    // 3 digits
-    if (hex.length == 4) {
-    r = "0x" + hex[1] + hex[1];
-    g = "0x" + hex[2] + hex[2];
-    b = "0x" + hex[3] + hex[3];
-
-    // 6 digits
-    } else if (hex.length == 7) {
-    r = "0x" + hex[1] + hex[2];
-    g = "0x" + hex[3] + hex[4];
-    b = "0x" + hex[5] + hex[6];
-    }
-
-    return "rgb("+ + decreaser(r) + "," + + decreaser(g) + "," + + decreaser(b) + ")";
-} // verwiesen wird hier wieder auf css-tricks
-
-function decreaser(val) {
-    if (val !== 0 && val >= 50) {
-        val = val - 50;
-    } else if (val === val) {
-        val = 0;
-    }
-    return val;
+function hex(x) { // other function for rgb to hex just with one value yet
+    let hexD = colorConvert.getHexChars();
+    return isNaN(x) ? "00" : hexD[(x - x % 16) / 16] + hexD[x % 16];
 }
 
 //---------------[End of ColorGenCode]---------------
 
-function black() {
-    const controlColor = '#272727';
-    $('button').css('color', controlColor);
-    $('section p').css('color', controlColor);
-    $('.cTwo').css('color', controlColor);
-    $('.cTwo').css('border-color', controlColor);
-    $('.btnColorGen').css('border-color', controlColor);
-    $('button:first-child').css('border-color', controlColor);
-    $('.addBtn').css('border-color', controlColor);
-}
-
-function white() {
-    const controlColor = 'white';
+function ColorMode(color) {
+    const controlColor = color; //'#272727'
     $('button').css('color', controlColor);
     $('section p').css('color', controlColor);
     $('.cTwo').css('color', controlColor);
@@ -116,7 +175,7 @@ let colorPnl = {
         return '\n\t<section class="btnColorGen">' + this.btnColorGen + '\n\t\t<p> Color </p>' + '\n\t</section>';    
     },
     get btnColorGen() {
-        return '\n\t\t<button onclick="RanColorGen(\'.pnl-' + this.elmtCount + '\')"> Generieren </button>';
+        return '\n\t\t<button onclick="mainColor(\'.pnl-' + this.elmtCount + '\')"> Generieren </button>';
     },
     get sectionSign() {
         return '\n\t<div id="containerCTwo">\n\t\t' + '<p class="cTwo"> Color 2 </p>' + '\n\t</div>';
@@ -135,7 +194,7 @@ function addPnl() {
         $('.addPnl').before(colorPnl.container);
     }
     calcWidth();
-    white();
+    ColorMode('white');
     $('.pnl-' + (colorPnl.elmtCount - 1) + ' .btnColorGen button').click();
 }
 
@@ -146,7 +205,7 @@ function delPnl(delPnlID) {
         let targetPnl = $(this);
         targetPnl.removeClass('pnl-' + (indexer + 1) +'').addClass('pnl-' + indexer + '')
         targetPnl.find('.delete').attr('onclick', 'delPnl(' + indexer + ')');
-        targetPnl.find('.btnColorGen button').attr('onclick', 'RanColorGen(".pnl-' + indexer + '")');
+        targetPnl.find('.btnColorGen button').attr('onclick', 'mainColor(".pnl-' + indexer + '")');
         indexer++;
     })
     calcWidth();
@@ -155,6 +214,6 @@ function delPnl(delPnlID) {
 // Shorthand for $( document ).ready()
 $(function() {
     $('.colorPnl, .addPnl').css('width', 100 / (colorPnl.elmtCount + 1) + '%')
-    RanColorGen('.pnl-0');
-    white();
+    mainColor('.pnl-0');
+    ColorMode('white');
 });
