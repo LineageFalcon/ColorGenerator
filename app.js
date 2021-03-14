3be56d4dd339e9f2568d9430ac50ff82556b86b8
Malcom Cook <68928999+LineageFalcon@users.noreply.github.com>
2021-03-14 01:58:01 +0100

    🔧: created a test class + 📝: updated start func
    🔧: Test class helps me to understand the referencing form obj properties and how that affects logic as well as the view.
    
    📝: Updated the document ready function by deleting old instances that are not needed because they are static and added a controller who now oversees the runtime and fullfills his names dutie

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    new viewHandler();
-    new colorConvert();
-    new colorPanel();
+    new controller();
+
+    controller.setContainerNode('container');
+    controller._colorPanel = [];
+    controller.main();
 });
+
+class test {//test to understand the referencing of js objects
+    constructor() {
+        this.elementOne = document.createElement('div');
+        this.elementTwo = document.createElement('p');
+
+        this.classOne = 'xavier';
+    }
+
+    bind() {
+        this.elementOne.append(this.elementTwo);
+        this.elementOne.classList.add(this.classOne);
+
+        return this.elementOne;
+    }
+
+    change(value) {
+        this.elementOne.classList.add(value);
+    }
+
+    insert() {
+        document.querySelector('body').appendChild(this.elementOne);
+    }
+
+    eventListener() {
+        
+    }
+}
