df04b7679f2083c7e8c2f1289f4f9284b1b60137
Malcom Cook <68928999+LineageFalcon@users.noreply.github.com>
2021-03-14 15:03:28 +0100

    📝:  deleted the test class

--- app.js
+++ app.js
@@ -5,31 +5,3 @@ document.addEventListener("DOMContentLoaded", function() {
     controller._colorPanel = [];
     controller.main();
 });
-
-class test {//test to understand the referencing of js objects
-    constructor() {
-        this.elementOne = document.createElement('div');
-        this.elementTwo = document.createElement('p');
-
-        this.classOne = 'xavier';
-    }
-
-    bind() {
-        this.elementOne.append(this.elementTwo);
-        this.elementOne.classList.add(this.classOne);
-
-        return this.elementOne;
-    }
-
-    change(value) {
-        this.elementOne.classList.add(value);
-    }
-
-    insert() {
-        document.querySelector('body').appendChild(this.elementOne);
-    }
-
-    eventListener() {
-        
-    }
-}
