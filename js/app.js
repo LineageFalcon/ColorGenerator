a7ef121660495ea96c4c1d3682f2980963f1529d
Malcom Cook <thoriumblack@gmail.com>
2021-04-03 23:22:16 +0200

    📝: method renaming, reordering and cleaning

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {//poses as main function call and calls the controllers main function
-    new controllerOld('container', new Set(), 770);
     controller.main();
+    new controllerOld('container', new Set(), 770);
 });
