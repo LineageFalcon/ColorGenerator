67ac28c790ce775d64e3166c7477f484214b3ab4
Malcom Cook <thoriumblack@gmail.com>
2021-04-03 23:22:46 +0200

    📝: deleted replaced code

--- js/dynamics.js
+++ js/dynamics.js
@@ -56,9 +56,6 @@ class colorPanelEventHandler {
 				case 'delPnl':
 				    this.delPnl(arrayItem, panelListItem);
 					break;
-				case 'genColor':
-				    this.genColor(arrayItem, panelListItem);
-					break;
                 case 'dragPnl':
                     this.setDragAndDropEvent(panelListItem);
                     break;
@@ -74,12 +71,6 @@ class colorPanelEventHandler {
     delPnl(arrayItem, panelListItem) {
         arrayItem.addEventListener('click', () => {
             this._controller.deletePanel(panelListItem);
-		});
-    }
-
-    genColor(arrayItem, panelListItem) {
-        arrayItem.addEventListener('click', () => {
-            this._controller.newColors(panelListItem);
 		});
     }
 
@@ -99,126 +90,6 @@ class colorPanelEventHandler {
         window.addEventListener('resize', () => {
             this._controller.checkDevice();
         });
-    }
-}
-class colorPanel {//view
-    constructor() {
-        this.colorPanelDOMElements = {
-            delBtn:  document.createElement('button'), 
-            colorBtn: document.createElement('button'),
-            dragBtn: document.createElement('button'),
-            cTwoBtn: document.createElement('button'),
-            cOneBtn: document.createElement('button'),
-            delBtnIcon: document.createElement('i'),
-            dragBtnIcon: document.createElement('i'),
-            toolTipOne: document.createElement('span'),
-            toolTipTwo: document.createElement('span'),
-            container: document.createElement('div'),
-            colorOneElem: document.createElement('div'),
-            colorTwoElem: document.createElement('div')
-        }
-        
-        this.colorPanelElementClasses = {
-            containerClass: 'colorPnl', 
-            colorWrapperClass: ['colorOne', 'colorTwo'],
-            delBtnClass: 'delPnl',
-            btnIconClass: 'material-icons',
-            genBtnClass: 'genColor',
-            colorOneTextBtnClass: 'cOne',
-            colorTwoTextBtnClass: 'cTwo',
-            toolTipClass: 'tooltip',
-            dragBtnClass: 'dragPnl'
-        }
-
-        this.colorPanelElementColors = {
-            colorOne: '#aaaaaa',
-            colorTwo: '#555555',
-            colorLabelOne: '#222222',
-            colorLabelTwo: '#dddddd'
-        }
-
-        this._panelAttributeOrder = 1;
-
-        this._combinedColorPanel = null;
-
-        this.combinePanelElements();
-        this.generateColors();
-        this.setColors();
-    }
-
-    //#region Accessors
-        set CombinedColorPanel(cColorPanel) {
-            this._combinedColorPanel = cColorPanel;
-        }
-
-        get CombinedColorPanel() {
-            return this._combinedColorPanel;
-        }
-
-        set PanelAttributeOrder(order) {
-            this._panelAttributeOrder = order;
-            this.colorPanelDOMElements.container.style.order = order;
-        }
-
-        get PanelAttributeOrder() {
-            return this._panelAttributeOrder;
-        }
-    //#endregion Accessors
-
-    combinePanelElements() {
-        this.colorPanelDOMElements.container.append(this.colorPanelDOMElements.colorTwoElem);
-        this.colorPanelDOMElements.container.append(this.colorPanelDOMElements.colorOneElem);
-        this.colorPanelDOMElements.container.append(this.colorPanelDOMElements.colorBtn);
-        this.colorPanelDOMElements.container.append(this.colorPanelDOMElements.delBtn);
-            this.colorPanelDOMElements.delBtn.append(this.colorPanelDOMElements.delBtnIcon);
-                this.colorPanelDOMElements.delBtnIcon.textContent = 'remove_circle_outline';
-        this.colorPanelDOMElements.container.append(this.colorPanelDOMElements.dragBtn);
-            this.colorPanelDOMElements.dragBtn.append(this.colorPanelDOMElements.dragBtnIcon);
-                this.colorPanelDOMElements.dragBtnIcon.textContent = 'drag_handle';
-        this.colorPanelDOMElements.container.append(this.colorPanelDOMElements.cOneBtn);
-            this.colorPanelDOMElements.cOneBtn.append(this.colorPanelDOMElements.toolTipOne);
-        this.colorPanelDOMElements.container.append(this.colorPanelDOMElements.cTwoBtn);
-            this.colorPanelDOMElements.cTwoBtn.append(this.colorPanelDOMElements.toolTipTwo);
-
-        this.colorPanelDOMElements.container.classList.add(this.colorPanelElementClasses.containerClass);
-        this.colorPanelDOMElements.colorOneElem.classList.add(this.colorPanelElementClasses.colorWrapperClass[0]);
-        this.colorPanelDOMElements.colorTwoElem.classList.add(this.colorPanelElementClasses.colorWrapperClass[1]);
-        this.colorPanelDOMElements.delBtn.classList.add(this.colorPanelElementClasses.delBtnClass);
-            this.colorPanelDOMElements.delBtnIcon.classList.add(this.colorPanelElementClasses.btnIconClass);
-        this.colorPanelDOMElements.dragBtn.classList.add(this.colorPanelElementClasses.dragBtnClass);
-            this.colorPanelDOMElements.dragBtnIcon.classList.add(this.colorPanelElementClasses.btnIconClass)
-        this.colorPanelDOMElements.colorBtn.classList.add(this.colorPanelElementClasses.genBtnClass);
-        this.colorPanelDOMElements.cOneBtn.classList.add(this.colorPanelElementClasses.colorOneTextBtnClass);
-            // this.colorPanelDOMElements.toolTipOne.classList.add(this.colorPanelElementClasses.toolTipClass);
-        this.colorPanelDOMElements.cTwoBtn.classList.add(this.colorPanelElementClasses.colorTwoTextBtnClass);
-            // this.colorPanelDOMElements.toolTipTwo.classList.add(this.colorPanelElementClasses.toolTipClass);
-
-        this._combinedColorPanel = this.colorPanelDOMElements.container;
-    }
-
-    generateColors() {
-        this.colorPanelElementColors.colorOne = colorConvert.ranColorGen();
-        this.colorPanelElementColors.colorTwo = colorConvert.hslToHex(colorConvert.changeLightness(colorConvert.hexToHsl(this.colorPanelElementColors.colorOne), -20));
-        this.colorPanelElementColors.colorLabelOne = colorConvert.hslToHex(colorConvert.changeLightness(colorConvert.hexToHsl(this.colorPanelElementColors.colorOne), -20));
-        this.colorPanelElementColors.colorLabelTwo = colorConvert.hslToHex(colorConvert.changeLightness(colorConvert.hexToHsl(this.colorPanelElementColors.colorOne), 15));
-    }
-
-    setColors() {//rename funciton //viewModel
-        this.colorPanelDOMElements.colorOneElem.style.backgroundColor = this.colorPanelElementColors.colorOne;
-        this.colorPanelDOMElements.colorTwoElem.style.backgroundColor = this.colorPanelElementColors.colorTwo;
-        this.colorPanelDOMElements.colorOneElem.style.boxShadow = this.colorPanelElementColors.colorOne + ' 0 0 0 1px';
-        this.colorPanelDOMElements.colorTwoElem.style.boxShadow = this.colorPanelElementColors.colorTwo + ' 0 0 0 1px';
-        console.log(this.colorPanelElementColors.colorTwo + ' 0 0 0 1px');
-    
-        this.colorPanelDOMElements.cOneBtn.textContent = this.colorPanelElementColors.colorOne;
-        this.colorPanelDOMElements.cTwoBtn.textContent = this.colorPanelElementColors.colorTwo;
-
-        this.colorPanelDOMElements.cOneBtn.style.color = this.colorPanelElementColors.colorLabelOne;
-        this.colorPanelDOMElements.dragBtn.style.color = this.colorPanelElementColors.colorLabelOne;
-        this.colorPanelDOMElements.cTwoBtn.style.color = this.colorPanelElementColors.colorLabelTwo;
-        
-        this.colorPanelDOMElements.delBtn.style.color = this.colorPanelElementColors.colorOne;
-        this.colorPanelDOMElements.delBtn.style.backgroundColor = this.colorPanelElementColors.colorTwo;
     }
 }
 
@@ -259,17 +130,9 @@ class controllerOld {
         this.colorPanel;
     }
 
-    newColors(panelListItem) {
-        for (let panel of this._colorPanel) {
-            if(panel._combinedColorPanel === panelListItem._combinedColorPanel) {
-                panel.generateColors();
-                panel.setColors();
-            }
-        }
-    }
-
     pushNewPanel() {
         let panelListItem = new colorPanel();
+        
         let cPanel = panelListItem.CombinedColorPanel;
         
         console.log(this._colorPanel.size);
@@ -277,6 +140,7 @@ class controllerOld {
         
         document.getElementById(this._containerNode).insertAdjacentElement( 'beforeend', cPanel);//seperate function
         this._colorPanel.add(panelListItem);
+        viewHandler._colorPanelList.add(panelListItem);
         this._colorPanelEventHandler.bindPanelButtons(panelListItem);
         this.checkDevice();
     }
