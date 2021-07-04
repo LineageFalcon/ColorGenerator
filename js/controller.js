57c82853df1295d558bbd0d3a34b9861db373be6
Malcom Cook <68928999+LineageFalcon@users.noreply.github.com>
2021-07-04 18:12:15 +0200

    📝: implementation of drag and drop event

--- js/controller.js
+++ js/controller.js
@@ -3,9 +3,10 @@ class controller {
 
     static main() {
         viewHandler.loadView();
-        viewHandler._colorPanelContainerNode.onclick = (event) => {eventHandler.delegateClickEvent(event);};
-        viewHandler._addPanelButton.onclick = () => {viewHandler.addPanel();};
-        window.onresize = () => {viewHandler.checkOrientation()};
+        viewHandler._colorPanelContainerNode.onclick = (event) => {eventHandler.delegateClickEvent(event);}
+        viewHandler._colorPanelContainerNode.onpointerdown = (event) => {eventHandler.delegatePointerDownEvent(event);}
+        viewHandler._addPanelButton.onclick = () => {viewHandler.addPanel();}
+        window.onresize = () => {viewHandler.checkOrientation()}
 
         viewHandler.addPanel();
     }
