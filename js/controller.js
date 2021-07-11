04c8e0352714c86a084475ca2e6acb3945474d69
Malcom Cook <68928999+LineageFalcon@users.noreply.github.com>
2021-07-11 02:10:21 +0200

    📝: added DnD loading function

--- js/controller.js
+++ js/controller.js
@@ -2,10 +2,11 @@ class controller {
     constructor() {}
 
     static main() {
-        viewHandler.loadView();
-        viewHandler._colorPanelContainerNode.onclick = (event) => {eventHandler.delegateClickEvent(event);}
-        viewHandler._colorPanelContainerNode.onpointerdown = (event) => {eventHandler.delegatePointerDownEvent(event);}
-        viewHandler._addPanelButton.onclick = () => {viewHandler.addPanel();}
+        viewHandler.load();
+        dragAndDrop.load(viewHandler._colorPanelContainerNode, viewHandler._dragStyleClass);
+        viewHandler._colorPanelContainerNode.onclick = (event) => {eventHandler.delegateClickEvent(event)}
+        viewHandler._colorPanelContainerNode.onpointerdown = (event) => {eventHandler.delegatePointerDownEvent(event)}
+        viewHandler._addPanelButton.onclick = () => {viewHandler.addPanel()}
         window.onresize = () => {viewHandler.checkOrientation()}
 
         viewHandler.addPanel();
