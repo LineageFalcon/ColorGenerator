9ba748a558eac84654f948044f0692a4e1d2641b
Malcom Cook <thoriumblack@gmail.com>
2021-07-13 00:30:55 +0200

    📝: reorderd some functions

--- js/controller.js
+++ js/controller.js
@@ -5,8 +5,8 @@ class controller {
         viewHandler.load();
         dragAndDrop.load(viewHandler._colorPanelContainerNode, viewHandler._dragStyleClass);
         viewHandler._colorPanelContainerNode.onclick = (event) => {eventHandler.delegateClickEvent(event)}
-        viewHandler._colorPanelContainerNode.onpointerdown = (event) => {eventHandler.delegatePointerDownEvent(event)}
         viewHandler._addPanelButton.onclick = () => {viewHandler.addPanel()}
+        viewHandler._colorPanelContainerNode.onpointerdown = (event) => {eventHandler.delegatePointerDownEvent(event)}
         window.onresize = () => {viewHandler.checkOrientation()}
 
         viewHandler.addPanel();
